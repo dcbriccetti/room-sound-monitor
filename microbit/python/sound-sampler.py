1399b19ebcadfa8f85a4d709bc3bcb71bd5674c2
Dave Briccetti <daveb@davebsoft.com>
2025-04-06 21:50:54 -0700

    Encapsulate some sound-sampler code into a Location class

--- microbit/python/sound-sampler.py
+++ microbit/python/sound-sampler.py
@@ -3,22 +3,30 @@ import radio
 
 TILT_SENSITIVITY = 0.01  # Something between 0 and 1 to slow down a bit
 adjust_mode = True
-x = 2.0
-y = 2.0
-prev_x = 0
-prev_y = 0
 
-def show_coords():
-    global prev_x, prev_y
-    if round(x) == prev_x and round(y) == prev_y:
-        return
-    display.set_pixel(prev_x, prev_y, 0)
-    display.set_pixel(round(x), round(y), 9)
-    prev_x = round(x)
-    prev_y = round(y)
+class Location:
+    def __init__(self):
+        self.x = 2.0
+        self.y = 2.0
+        self.prev_x = 0
+        self.prev_y = 0
 
-def update_coord(current, delta, lower=0, upper=4):
-    return min(max(current + delta, lower), upper)
+    def show_coords(self):
+        if round(self.x) == self.prev_x and round(self.y) == self.prev_y:
+            return
+        display.set_pixel(self.prev_x, self.prev_y, 0)
+        display.set_pixel(round(self.x), round(self.y), 9)
+        self.prev_x = round(self.x)
+        self.prev_y = round(self.y)
+
+    def update_from_tilt(self):
+        self.x = self.constrain(self.x + accelerometer.get_x() / 1023 * TILT_SENSITIVITY)
+        self.y = self.constrain(self.y + accelerometer.get_y() / 1023 * TILT_SENSITIVITY)
+        self.show_coords()
+
+    @staticmethod
+    def constrain(value, lower=0, upper=4):
+        return min(max(value, lower), upper)
 
 def display_level(level):
     num_graph_leds = round(level / 255.0 * 25)
@@ -28,6 +36,7 @@ def display_level(level):
             display.set_pixel(x, y, 5 if led_num <= num_graph_leds else 0)
             led_num += 1
 
+loc = Location()
 radio.on()
 radio.config(group=1)
 
@@ -39,20 +48,15 @@ while True:
 
     # If adjust mode is on, update coordinates based on tilt.
     if adjust_mode:
-        x = update_coord(x, accelerometer.get_x() / 1023 * TILT_SENSITIVITY)
-        y = update_coord(y, accelerometer.get_y() / 1023 * TILT_SENSITIVITY)
-        show_coords()
-    else:
-        level = microphone.sound_level()
-        display_level(level)
-        message = ','.join(str(item) for item in
-                           (round(x),
-                            round(y),
-                            level,
-                            ))
-        if message != last_message:
-            last_message = message
-            print(message)
-            radio.send(message)
+        loc.update_from_tilt()
+        continue
 
-        sleep(50)
+    level = microphone.sound_level()
+    display_level(level)
+    message = ','.join(str(item) for item in (round(loc.x), round(loc.y), level))
+    if message != last_message:
+        last_message = message
+        print(message)
+        radio.send(message)
+
+    sleep(50)
